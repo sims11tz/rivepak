fb20cfc49622ec3cccbbc9110bf9937945b679c6
Dave Geurts <dgeurts@Dave-Geurtss-Mac-Studio.local>
2025-09-05 17:02:09 -0600

    chore: stop tracking build artifacts

--- dist/canvasObjects/CanvasContainer.d.ts
+++
@@ -1,6 +0,0 @@
-import { CanvasObj, CanvasObjectDef } from "./CanvasObj";
-export declare class CanvasContainerObj extends CanvasObj {
-    constructor(canvasDef: CanvasObjectDef);
-    Update(time: number, frameCount: number, onceSecond: boolean): void;
-    Dispose(): void;
-}
--- dist/canvasObjects/TextObj.d.ts
+++
@@ -1,8 +0,0 @@
-import { CanvasPixiShapeObj } from "./CanvasPixiShapeObj";
-import { CanvasObjectDef } from "./CanvasObj";
-export declare class TextObject extends CanvasPixiShapeObj {
-    constructor(canvasDef: CanvasObjectDef);
-    DrawVectors(): void;
-    Update(time: number, frameCount: number, onceSecond: boolean): void;
-    Dispose(): void;
-}
