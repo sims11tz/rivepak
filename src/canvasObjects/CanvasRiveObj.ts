a3bc6f83569bbde20ec13a6c8e1f97ecac2ec08c
Dave Geurts <davewgeurts@gmail.com>
2025-12-01 08:53:27 -0700

    ok go

--- src/canvasObjects/CanvasRiveObj.ts
+++ src/canvasObjects/CanvasRiveObj.ts
@@ -1605,7 +1605,7 @@ export class CanvasRiveObj extends BaseCanvasObj
 				}
 			}
 
-// Debug: Log state changes
+// Debug: Log state c hanges
 			//if(!this._disposed && this._stateMachine)
 			//{
 			//	const stateChangeCount = this._stateMachine.stateChangedCount();
